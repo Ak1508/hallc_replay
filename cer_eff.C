379cc0e049ad76e254b876cd2c9fb9e4ca12940b
hallc-online <hallconline@gmail.com>
2018-04-11 15:07:28 -0400

    got the new cer_eff

--- cer_eff.C
+++ cer_eff.C
@@ -9,6 +9,8 @@ void cer_eff()
    TH1F *calHist = new TH1F("h2", "cal", 100, 0.0, 2.0);
    TH1F *W2Hist = new TH1F("h3", "W2", 100, 0.001, 5.0);
    TH1F *npeHist = new TH1F("h4", "npeSum", 100, 0, 10);
+   TH1F *h1 = new TH1F("hist1", "", 100, 0, 5);
+   TH1F *h2 = new TH1F("hist2", "", 100, 0, 5);
 
 
    TTreeReaderValue<Double_t> delta(myReader, "P.gtr.dp");
@@ -19,27 +21,38 @@ void cer_eff()
 
    TTreeReaderValue<Double_t> npe(myReader, "P.ngcer.npeSum");
 
-    while (myReader.Next()) 
+   while (myReader.Next()) 
       {
 	
-		if (*cal>0.001)
-	  {
-	    calHist->Fill(*cal);
-	     }
-	
-	deltaHist->Fill(*delta);
-	
-	//	x_jHist->Fill(*x_j);
-	if(*npe>0)
-	  {
-	    npeHist->Fill(*npe);
-	  }
-	W2Hist->Fill(*W2);
-	
-      }//end of while
+	if (*cal>0.001)
+	      {
+		calHist->Fill(*cal);
+	      }
+	    
+	    deltaHist->Fill(*delta);
+	    
+	    //	x_jHist->Fill(*x_j);
+	    if(*npe>0)
+	      {
+		npeHist->Fill(*npe);
+	      }
+	    W2Hist->Fill(*W2);
+	    
+	    if (*delta>-10.0 && *delta <20.0)
+	      {
+		h1->Fill(*cal);
+		
+		if (*cal>0.8 && *npe>3 && *W2>0.8 && *W2< 1.5)
+		  {
+		    h2->Fill(*cal);
+		  }
+	      }//end of delta
 
+	  	
+      }//end of while
+    
     TCanvas *c1 = new TCanvas ("c1","  ", 400, 400);
-    c1->Divide(2,2);
+    c1->Divide(3,2);
     
     c1->cd(1);
     calHist->Draw();
@@ -47,11 +60,16 @@ void cer_eff()
     c1->cd(2);
     deltaHist->Draw();
     
-     c1->cd(3);
-     W2Hist->Draw();
+    c1->cd(3);
+    W2Hist->Draw();
     
     c1->cd(4);
     npeHist->Draw();
-   
-   
+
+    double deno_elec = h1->GetEntries();
+    double num_elec = h2->GetEntries();
+ 
+    cout <<"eff :"<< num_elec/deno_elec <<endl;
+    
+    
 }//end of void
